a6fc5a0bc323270dd81850060f1fe6de5477d094
Nico Geyso <nico.geyso@fu-berlin.de>
2015-12-19 12:31:17 +0100

    add leaflet-draw to map
    * new geojson objects can be added through leaflet-draw
    * data will be stored in a FeatureGroup
    * no persistence with the backend at the moment
    
    based on
    https://github.com/tombatossals/angular-leaflet-directive/blob/master/examples/0401-controls-draw-example.html

--- app/js/settings.js
+++ app/js/settings.js
@@ -7,5 +7,9 @@ mapApp
   .constant('defaults', {
       lat: 51.505,   // London
       lng: -0.09,    // London
-      zoom: 11
+      zoom: 11,
+      marker : {
+        iconSize: 24,
+        iconUrl : 'img/logos/24x24/attention.png'
+      }
    })
